c288747fd98039d2aeb9fb9f4d6fea5a43bc904a
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2024-05-10 16:40:20 +0400

    Create topic action impl  ans save in db

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,79 @@
 "use server";
 
-export async function createTopic() {
-  // TODO revalidate homepage
+import type { Topic } from "@prisma/client";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import paths from "@/paths";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Only lowercase letters and dashes without spaces are allowed",
+    }),
+  description: z.string().min(10).max(255),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be logged in to create a topic"],
+      },
+    };
+  }
+
+  let topic: Topic | null = null;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["An unknown error occurred"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.home());
+  redirect(paths.topicShow(topic.slug));
 }
