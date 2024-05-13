afc093a9ace9f04f16d8a984f0d6ff08938dadc9
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2024-05-13 16:52:40 +0400

    add comments, list posts

--- page.tsx
+++ page.tsx
@@ -3,7 +3,6 @@ import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
-import { fetchCommentsByPostId } from "@/db/queries/comments";
 
 interface PostShowPageProps {
   params: {
@@ -14,14 +13,15 @@ interface PostShowPageProps {
 
 export default async function PostShowPage({ params }: PostShowPageProps) {
   const { slug, postId } = params;
+
   return (
     <div className="space-y-3">
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
-      <CommentCreateForm postId={postId} startOpen />
-      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
     </div>
   );
 }
---
+++ src/db/queries/comments.ts
@@ -0,0 +1,16 @@
+import type { Comment } from "@prisma/client";
+import { db } from "@/db";
+
+export type CommentWithAuthor = Comment & {
+  user: { name: string | null; image: string | null };
+};
+
+export function fetchCommentsByPostId(
+  postId: string
+): Promise<CommentWithAuthor[]> {
+  return db.comment.findMany({
+    where: { postId },
+    include: { user: { select: { name: true, image: true } } },
+  });
+}
+// Compare this snippet from src/db/queries/topics.ts:
